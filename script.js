8696c0203a909ce36e3e642d9f99a184d7631754
Paul Chang Fatt <p.changfatt@gmail.com>
2019-12-07 02:54:32 -0500

    Connected to the sql database + Dynamic dropdown list

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
-
-    getLatin();
+getLatin();
     function getLatin()
     {
-        $.get("/", function(data){
-            if(!data)
+        $.get("/latin", function(sqlData){
+            if(!sqlData)
             {
                 console.log("No data received");
             }
             console.log("Received data");
-            for(var i = 0; i < data.length; i++)
-                console.log(data[i].genus);
+            // for(var i = 0; i < sqlData.length; i++)
+            //     showLatin(sqlData[i].GENUS + " " + sqlData[i].SPECIES);
+            showLatin(sqlData)
         });
     }
 
-});
+    function showLatin(data)
+    {
+        console.log(data);
+        var LatinMenu = "";
+         for(var i = 0; i<data.length; i++)
+         {
+            LatinMenu += "<a class=\"dropdown-item\" href=\"#\">" + data[i].GENUS + " " + data[i].SPECIES + "</a>\n";
+             // $("#LatinDropDownMenu").each(function(){
+             //     $(this).wrapInner(LatinMenu);
+             // })
+        }
+        $("#LatinDropDownMenu").append(LatinMenu);
+        $('.dropdown-menu a').click(function(){
+            $('#selected').text($(this).text());
+        });
+    }
+
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('db/flowers2019.db');
 var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(3000, function(){
@@ -11,5 +12,15 @@ app.listen(3000, function(){
 });
 
 app.get('/', function (request, response) {
-    response.send("Hello World")
+    response.send("Hello World");
+});
+
+app.get('/latin', function(request, response){
+   console.log('GET request received at /latin');
+   db.all('SELECT genus, species FROM FLOWERS', function (err, rows) {
+        if(err)
+            console.log("Error: " + err);
+        else
+            response.send(rows);
+   })
 });
